5fc5a78f8e1c852ba25657393ebd7be9401be3bf
Evan Lloyd <evan.u.lloyd@gmail.com>
2014-11-08 09:59:52 -0500

    Fix capitalization issue

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
-app.import('vendor/chordsJS/chords.js');
+app.import('vendor/chordsJs/chords.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
