319a3568ad3e32337bd0bfa1030be071ec3bace2
Evan Lloyd <evan.u.lloyd@gmail.com>
2014-11-08 08:39:29 -0500

    Add call to render component

--- app/components/guitar-chords.js
+++ app/components/guitar-chords.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  name: 'D',
+  positions: 'xx0232',
+  fingers: '---132',
+  size: '3',
+  didInsertElement: function() {
+    chords.replace();
+    console.log('hi')
+  }
 });
