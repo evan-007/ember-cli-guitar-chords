5c900247ca94ddab6bff4e7b8a0ac741b65d643e
Evan Lloyd <evan.u.lloyd@gmail.com>
2014-11-09 09:01:38 -0500

    Remove chordsJS

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
   name: 'ember-cli-guitar-chords',
 
   included: function(app) {
-    app.import('vendor/chordsJs/chords.js');
     app.import('vendor/audiosynth/audiosynth.js ')
     app.import('vendor/chordography/chordography.js')
     app.import('vendor/chordography/chordography.css')
